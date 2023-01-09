e46e56c8e12ab432673971e03f60423c86a74256
Alimerdan Rahimov <mirabror.mirzokhidov@endava.com>
2023-01-09 20:47:05 +0000

    create doc

--- rest_webform/api.py
+++ rest_webform/api.py
@@ -2,5 +2,9 @@ import frappe,json
 
 @frappe.whitelist(allow_guest=True)
 def post_test(**kwargs):
+    doc = frappe.new_doc('hi')
+    doc.hi = 'asds'
+    doc.insert(ignore_permissions=True)
+    frappe.db.commit()
     kwargs=frappe._dict(kwargs) 
     return str(kwargs)
