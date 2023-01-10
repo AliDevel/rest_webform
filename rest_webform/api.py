051bffd951f3f3ea6e521e33461e8dcd8b138074
Alimerdan Rahimov <mirabror.mirzokhidov@endava.com>
2023-01-10 18:48:43 +0000

    changes

--- rest_webform/api.py
+++ rest_webform/api.py
@@ -5,24 +5,25 @@ def post_test(**kwargs):
     kwargs=frappe._dict(kwargs) 
    
     doc = frappe.new_doc('Lead')
-  # address_dic= kwargs['adresse']
+    address_dic= kwargs['adresse']
     title = kwargs['titel']
     name= kwargs['vorname']
     last_name =  kwargs['nachname']
-   # address= address_dic['address']
-    #city= address_dic['city']
-    #zip = address_dic['postal_code']
-    #country = address_dic['country']
-    #state = address_dic['state_province']
+    address= address_dic['address']
+    city= address_dic['city']
+    zip = address_dic['postal_code']
+    country = address_dic['country']
+    state = address_dic['state_province']
     email= kwargs['e_mail']
     phone = kwargs['telefon']
+    company = kwargs['firma']
     description = kwargs['beschreibung']
-    
+    doc.salutation = title 
     doc.first_name= name
     doc.last_name = last_name
     doc.email_id = email
     doc.phone = phone 
-    doc.company_name = 'None'
+    doc.company_name = company
 
     doc.insert(ignore_permissions=True)
     frappe.db.commit()
