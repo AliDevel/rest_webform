0d41b93e2d472695d43f8e0d9dd377dfa74dfb5e
Alimerdan Rahimov <mirabror.mirzokhidov@endava.com>
2023-01-10 18:42:48 +0000

    changes

--- rest_webform/api.py
+++ rest_webform/api.py
@@ -2,21 +2,21 @@ import frappe,json
 
 @frappe.whitelist(allow_guest=True)
 def post_test(**kwargs):
-    kwargs=frappe._dict(kwargs) 
+    #kwargs=frappe._dict(kwargs) 
    
     doc = frappe.new_doc('Lead')
   # address_dic= kwargs['adresse']
-    title = kwargs['titel']
-    name= kwargs['vorname']
-    last_name =  kwargs['nachname']
+#    title = kwargs['titel']
+ #   name= kwargs['vorname']
+  #  last_name =  kwargs['nachname']
    # address= address_dic['address']
     #city= address_dic['city']
     #zip = address_dic['postal_code']
     #country = address_dic['country']
     #state = address_dic['state_province']
-    email= kwargs['e_mail']
-    phone = kwargs['telefon']
-    description = kwargs['beschreibung']
+   # email= kwargs['e_mail']
+    #phone = kwargs['telefon']
+    #description = kwargs['beschreibung']
     
     # doc.first_name= name
     #doc.last_name = last_name
